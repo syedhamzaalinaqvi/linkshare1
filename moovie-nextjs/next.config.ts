49a5115d47d1896141558f7bed00252c4529dfa7
Syed Hamza Ali Naqvi <syedhamzaalinaqvi4@gmail.com>
2025-12-06 19:08:44 +0500

    Configure Next.js app to run at /moovie path

--- moovie-nextjs/next.config.ts
+++ moovie-nextjs/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: '/moovie',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
